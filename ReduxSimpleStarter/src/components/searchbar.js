20647207599f3d27ab0fbea99483dee0f1e1042c
Easton, Hayley <heaston@netsuite.com>
2017-06-19 13:33:05 +0100

    Section 1, lesson 18 complete

---
+++ ReduxSimpleStarter/src/components/searchbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+// const SearchBar = () => { // a functional component that needs refactoring into a class based component
+  // return <input />;
+// }
+// use a class based method instead only when you need extra functionality
+// if you just want to return the component, use the functional version above
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' }; // object {} with properties i.e. term, on the state
+    // only ever create state in constructor
+  }
+
+  render() { // a function
+    // return <input onChange={this.onInputChange} />;
+    // return <input onChange={event => console.log(event.target.value)} />;
+    return (// setState makes it re-render
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />
+        Value of the input: {this.state.term}
+      </div>
+    );
+  }
+
+  //onInputChange(event) {
+  //  console.log(event.target.value);
+  //}
+}
+
+export default SearchBar;
--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/searchbar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyBCVV-Fr5Jj4_p8I0cDMui6OYFXYYHSgpk';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a new component. This component should produce
+// some HTML
+// This is all ES6 code
+// const App = function() { // this is a class, not an instance
+const App = () => { // fat arrow identical to function keyword
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+// Take this component's generated HTML and put it
+// on the page (in the DOM)
+// ReactDOM.render(App);
+// you need to make an instance before passing
+// so you wrap App in JSX tags to make an instance of the component
+ReactDOM.render(<App />, document.querySelector('.container'));
